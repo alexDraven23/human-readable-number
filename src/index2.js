b0f882a85be0769df8c40360083fc5890f6543cd
Alexander Chentsov <alexchenc96@yandex.by>
2022-08-09 12:37:13 +0300

    feat: added index2.js as second solution

--- src/index2.js
+++ src/index2.js
@@ -1,4 +1,4 @@
-module.exports = function toReadable(number) {
+function toReadable(number) {
     let units = function (number) {
         switch (number) {
             case 1: return 'one';
@@ -12,6 +12,7 @@ module.exports = function toReadable(number) {
             case 9: return 'nine';
         }
     }
+
     let untilNineteen = function (number) {
         switch (number) {
             case 10: return 'ten';
@@ -29,9 +30,10 @@ module.exports = function toReadable(number) {
 
     let dozens = function (number) {
         switch (number) {
+            case 1: return 'ten';
             case 2: return 'twenty';
             case 3: return 'thirty';
-            case 4: return 'forty';
+            case 4: return 'furty';
             case 5: return 'fifty';
             case 6: return 'sixty';
             case 7: return 'seventy';
@@ -40,23 +42,9 @@ module.exports = function toReadable(number) {
         }
     }
 
-    let hundreds = function (number) {
-        switch (number) {
-            case 1: return 'one hundred';
-            case 2: return 'two hundred';
-            case 3: return 'three hundred';
-            case 4: return 'four hundred';
-            case 5: return 'five hundred';
-            case 6: return 'six hundred';
-            case 7: return 'seven hundred';
-            case 8: return 'eight hundred';
-            case 9: return 'nine hundred';
-        }
-    }
-
     if (number === 0) {
         return 'zero';
-    }
+    } 
     if (number < 10) {
         return units(number);
     }
@@ -64,29 +52,25 @@ module.exports = function toReadable(number) {
         return untilNineteen(number);
     }
     if (number <= 99) {
-        if (number % 10 === 0) {
-            return (dozens(Math.floor(number / 10)));
-        } else {
-            return (dozens(Math.floor(number / 10)) + ' ' + units(number % 10));
-        }
+      if (number % 10 === 0) {
+        return (dozens(Math.floor(number / 10)));
+      } else {
+        return (dozens(Math.floor(number / 10)) + ' ' + units(number % 10))
+      }
     }
     if (number <= 999) {
         if (number % 100 === 0) {
-            return (hundreds(Math.floor(number / 100)));
-        }
-        else if (number % 100 <= 9) {
-            return (hundreds(Math.floor(number / 100)) + ' ' + units(number % 10));
-        }
-        else if (number % 100 <= 19 && number % 100 >= 10) {
-            return (hundreds(Math.floor(number / 100)) + ' ' + untilNineteen(number % 100));
-        }
-        else if (number % 10 === 0) {
-            return hundreds(Math.floor(number / 100)) + ' ' + dozens((Math.floor((number % 100) / 10)))
+            return (units(Math.floor(number / 100)) + ' hundred');
+        } else if (number % 100 <= 9) {
+            return (units(Math.floor(number / 100)) + ' hundred ' + units(number % 100));
+        } else if (number % 100 <= 19) {
+            return (units(Math.floor(number / 100)) + ' hundred ' + untilNineteen(number % 100));
+        } else if (number % 10 === 0) {
+            return (units(Math.floor(number / 100)) + ' hundred ' + dozens((Math.floor((number % 100) / 10))))
         } else {
-            return (hundreds(Math.floor(number / 100)) + ' ' + dozens((Math.floor((number % 100) / 10))) + ' ' + units(number % 10));
+            return (units(Math.floor(number / 100)) + ' hundred ' + dozens((Math.floor((number % 100) / 10))) + ' ' + units(number % 10))
         }
+            
     }
 }
-
-
-
+console.log(toReadable(131))
